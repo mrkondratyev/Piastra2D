6374417a8fbd1bb2596bfd85047312480d1a4ed9
Ilya A. Kondratyev <mrkondratyev95@gmail.com>
2024-06-25 18:43:06 +0300

    Update CFL_condition.py
    extended the current CFL timestep calculation in order to take into account advection and MHD

--- CFL_condition.py
+++ CFL_condition.py
@@ -20,8 +20,7 @@ output:
 import numpy as np
 from ideal_hydro_functions import soundSpeed
 
-
-def CFLcondition(grid,fluid,eos):
+def CFLcondition_fluid(grid,fluid,eos,CFL):
       
     #sound speed calculation for whole domain
     sound = soundSpeed(fluid.dens, fluid.pres, eos)
@@ -31,6 +30,70 @@ def CFLcondition(grid,fluid,eos):
     dt2 = np.min( grid.dx2 / (1e-14 + np.abs(fluid.vel2) + sound) )
     
     #final timestep (it is already divided by two for 2D calculations for stability reasons)
-    dt = 0.5 * min(dt1, dt2)
+    dt = CFL * min(dt1, dt2)
+    
+    return dt
+
+
+
+"""
+This function calculates an avaliable timestep for magnetohydrodynamics simulation in 2D 
+with accordance to well-known CFL condition (Courant, Friedrichs and Lewy (1928))
+this conditions states, that during the simulation, the fastest wave in the system should not
+propagate further than one cell during the timestep
+
+input:
+    1) "grid" -- GRID class object
+    2) fluid -- fluid state class object
+    3) eos -- equation of state class object 
+output: 
+    dt -- avaliable timestep for 2D simulation according CFL stability condition
+
+@author: mrkondratyev
+"""
+
+def CFLcondition_mhd(grid,mhd,eos,CFL):
+      
+    Ngc = grid.Ngc
+    #sound speed calculation for whole domain
+    sound = soundSpeed(mhd.dens[Ngc:-Ngc, Ngc:-Ngc], mhd.pres[Ngc:-Ngc, Ngc:-Ngc], eos)
+    
+    #squared fast magnetosonic speed calculation for whole domain 
+    cf2 = sound**2 + (mhd.bfi1[Ngc:-Ngc, Ngc:-Ngc]**2 + mhd.bfi2[Ngc:-Ngc, Ngc:-Ngc]**2 + mhd.bfi3[Ngc:-Ngc, Ngc:-Ngc]**2)/(mhd.dens[Ngc:-Ngc, Ngc:-Ngc] + 1e-14)
+    
+    #maximal possible timestep in each direction
+    dt1 = np.min( grid.dx1[Ngc:-Ngc, Ngc:-Ngc] / (1e-14 + np.abs(mhd.vel1[Ngc:-Ngc, Ngc:-Ngc]) + np.sqrt(cf2)) )
+    dt2 = np.min( grid.dx2[Ngc:-Ngc, Ngc:-Ngc] / (1e-14 + np.abs(mhd.vel2[Ngc:-Ngc, Ngc:-Ngc]) + np.sqrt(cf2)) )
+    
+    #final timestep (it is already divided by two for 2D calculations for stability reasons)
+    dt = CFL * min(dt1, dt2)
+    
+    return dt
+
+
+"""
+This function calculates an avaliable timestep for linear advection simulation in 2D 
+with accordance to well-known CFL condition (Courant, Friedrichs and Lewy (1928))
+this conditions states, that during the simulation, the fastest wave in the system should not
+propagate further than one cell during the timestep
+
+input:
+    1) "grid" -- GRID class object
+    2) adv -- advected state class object
+output: 
+    dt -- avaliable timestep for 2D simulation according CFL stability condition
+
+@author: mrkondratyev
+"""
+
+
+def CFLcondition_adv(grid,adv,CFL):
+
+    #maximal possible timestep in each direction
+    dt1 = np.min( grid.dx1 / (1e-14 + np.abs(adv.vel1)) )
+    dt2 = np.min( grid.dx2 / (1e-14 + np.abs(adv.vel2)) )
+    
+    #final timestep (it is already divided by two for 2D calculations for stability reasons)
+    dt = CFL * min(dt1, dt2)
     
     return dt
