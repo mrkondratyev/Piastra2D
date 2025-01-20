7f4bea41a0cfccba1f530520006d35aa47ec7f80
Ilya A. Kondratyev <mrkondratyev95@gmail.com>
2025-01-20 19:18:14 +0300

    Update Riemann_fluxes.py
    added Roe solver for classical gas dynamics (no entropy fix)

--- Riemann_fluxes.py
+++ Riemann_fluxes.py
@@ -141,6 +141,101 @@ def Riemann_flux_nr_fluid(rhol, rhor, vxl, vxr, vyl, vyr, vzl, vzr, pl, pr, eos,
         Fetot = np.where(Sl >= 0.0, Fetot_L, 
                           np.where((Sl < 0.0) & (Sstar >= 0.0), Fetot_L + Sl * (etotS_L - etot_L),
                                    np.where((Sstar < 0.0) & (Sr >= 0.0), Fetot_R + Sr * (etotS_R - etot_R), Fetot_R)))
+
+
+    elif flux_type == 'Roe':
+    
+        # left and rigth enthalpies
+        entl = eos.GAMMA*pl/rhol/(eos.GAMMA-1.0) + (vxl**2 + vyl**2 + vzl**2)/2.0 
+        entr = eos.GAMMA*pr/rhor/(eos.GAMMA-1.0) + (vxr**2 + vyr**2 + vzr**2)/2.0 
+        
+        #1 Roe-averaged density
+        rhos = np.sqrt(rhol*rhor)
+        
+        # Roe-averaged velocity
+        vxs = (np.sqrt(rhol)*vxl + np.sqrt(rhor)*vxr)/(np.sqrt(rhol) + np.sqrt(rhor))
+        vys = (np.sqrt(rhol)*vyl + np.sqrt(rhor)*vyr)/(np.sqrt(rhol) + np.sqrt(rhor))
+        vzs = (np.sqrt(rhol)*vzl + np.sqrt(rhor)*vzr)/(np.sqrt(rhol) + np.sqrt(rhor))
+        
+        #Roe-averaged enthalpy
+        ents = (np.sqrt(rhol)*entl + np.sqrt(rhor)*entr)/(np.sqrt(rhol) + np.sqrt(rhor))
+        
+        #Roe-averaged sound speed 
+        #css = np.sqrt( (eos.GAMMA - 1.0)*(ents - vxs**2/2.0) )
+        #OR WE CAN USE THE FOLLOWING PRESCRIPTION
+        css = np.sqrt( (np.sqrt(rhol)*csl**2 + np.sqrt(rhor)*csr**2)/(np.sqrt(rhol) + np.sqrt(rhor)) + (eos.GAMMA - 1.0)*rhos/(np.sqrt(rhol) + np.sqrt(rhor))**2 * ((vxr-vxl)**2 + (vyr-vyl)**2 + (vzr-vzl)**2)/2.0 ) 
+        
+        #arrays of eugenvectors
+        rv = np.zeros((5,5,*rhos.shape))
+        rv[0,0,:,:] = np.ones_like(rhos)
+        rv[0,1,:,:] = vxs - css
+        rv[0,2,:,:] = vys
+        rv[0,3,:,:] = vzs
+        rv[0,4,:,:] = ents - vxs*css
+        
+        rv[1,0,:,:] = 2.0*np.ones_like(rhos)
+        rv[1,1,:,:] = 2.0*vxs
+        rv[1,2,:,:] = 2.0*vys
+        rv[1,3,:,:] = 2.0*vzs
+        rv[1,4,:,:] = vxs**2 + vys**2 + vzs**2
+        
+        rv[2,0,:,:] = np.zeros_like(rhos)
+        rv[2,1,:,:] = np.zeros_like(rhos)
+        rv[2,2,:,:] = 2.0*css
+        rv[2,3,:,:] = np.zeros_like(rhos)
+        rv[2,4,:,:] = 2.0*css*vys
+        
+        rv[3,0,:,:] = np.zeros_like(rhos)
+        rv[3,1,:,:] = np.zeros_like(rhos)
+        rv[3,2,:,:] = np.zeros_like(rhos)
+        rv[3,3,:,:] = 2.0*css
+        rv[3,4,:,:] = 2.0*css*vzs
+        
+        rv[4,0,:,:] = np.ones_like(rhos)
+        rv[4,1,:,:] = vxs + css
+        rv[4,2,:,:] = vys
+        rv[4,3,:,:] = vzs
+        rv[4,4,:,:] = ents + vxs*css
+        
+        #array of eugenvalues
+        eugen = np.zeros((5, *rhos.shape))
+        eugen[0,:,:] = np.minimum(np.abs(vxs - css), np.abs(vxl - csl))
+        eugen[1,:,:] = np.abs(vxs) 
+        eugen[2,:,:] = np.abs(vxs) 
+        eugen[3,:,:] = np.abs(vxs) 
+        eugen[4,:,:] = np.minimum(np.abs(vxs + css), np.abs(vxr + csr))
+        
+        #array of left eugenvectors residuals
+        dS = np.zeros((5, *rhos.shape))
+        dS[0,:,:] = ( (pr - pl) - rhos*css*(vxr - vxl) )/2.0/css**2
+        dS[1,:,:] = ( css**2*(rhor - rhos) - (pr - pl) )/2.0/css**2
+        dS[2,:,:] = rhos*css*(vyr - vyl)/2.0/css**2
+        dS[3,:,:] = rhos*css*(vzr - vzl)/2.0/css**2
+        dS[4,:,:] = ( (pr - pl) + rhos*css*(vxr - vxl) )/2.0/css**2
+        
+        #array of flux residuals
+        dF = np.zeros((5, *rhos.shape))
+        
+        dF[0, :, :] = (eugen[0,:,:]*rv[0,0,:,:]*dS[0,:,:] + eugen[1,:,:]*rv[1,0,:,:]*dS[1,:,:] + eugen[2,:,:]*rv[2,0,:,:]*dS[2,:,:] + eugen[3,:,:]*rv[3,0,:,:]*dS[3,:,:] + eugen[4,:,:]*rv[4,0,:,:]*dS[4,:,:])
+        
+        # Вычисление всех dF
+        dF = np.sum(eugen[:, np.newaxis, :, :] * rv[:, :, :, :] * dS[:, np.newaxis, :, :], axis=0)
+
+        
+        #final values of conservative fluxes
+        Fmass = (Fmass_L + Fmass_R)/2.0 - dF[0,:,:]/2.0
+        
+        Fmomx = (Fmomx_L + Fmomx_R)/2.0 - dF[1,:,:]/2.0
+
+        Fmomy = (Fmomy_L + Fmomy_R)/2.0 - dF[2,:,:]/2.0
+
+        Fmomz = (Fmomz_L + Fmomz_R)/2.0 - dF[3,:,:]/2.0
+
+        Fetot = (Fetot_L + Fetot_R)/2.0 - dF[4,:,:]/2.0
+        
+    else:
+    
+        raise ValueError(f"Unknown flux_type: {flux_type}. Expected one of ['LLF', 'HLL', 'HLLC', 'Roe'].")
     
     
     #check in what direction we solve the problem
